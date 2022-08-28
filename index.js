8892166cd555c6db822e5f2acbe345b573564017
Ahsan Raza <ahsanraza00059@gmail.com>
2022-08-28 14:30:27 +0500

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.post("/", (req, res) => {
 
     var bmi = Math.floor(w / (h * h));
 
-    res.send(`Your BMI is ${bmi}`);
+    res.send(`<p style="display:flex;align-items:center;justify-content:center;height:90vh;font-size:4rem;">Your BMI is ${bmi}</p>`);
 });
 
 var port = 3000;
