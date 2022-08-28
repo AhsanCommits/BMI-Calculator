d5bd4c1797465733a817f5746ce7bd1ff30f155b
Ahsan Raza <ahsanraza00059@gmail.com>
2022-08-28 10:23:22 +0500

    renamed index.html

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/bmiCalculator.html");
+    res.sendFile(__dirname + "/index.html");
 })
 
 app.post("/", (req, res) => {
